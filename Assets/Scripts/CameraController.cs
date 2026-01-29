ee47020a080e5bbbcd456cf81c6c68120a4fc00a
Winkhashtag <winkhashtag@gmail.com>
2026-01-29 15:29:41 -0500

    char + cam

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+	public float mouseSensitivity = 90;
+    private float xRotation;
+
+    private void Start()
+    {
+        xRotation = 0;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+
+        //get mouse input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //inverted on x axis
+        xRotation -= mouseY;
+
+        //xRotation should not ever be bigger than 90, or less than 90
+       xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        //rotate player around y axis, camera around x axis
+        transform.parent.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+ 
+	public float moveSpeed = 12;
+    private float gravity = 9.8f;
+    private float groundCheckRadius = 0.15f;
+
+    private CharacterController controller;
+
+    private bool isGrounded;
+    private Vector3 velocity;
+    private Transform feet;
+
+    public LayerMask groundLayer;
+
+    private void Awake()
+    {
+
+        controller = GetComponent<CharacterController>();
+    
+    feet = transform.Find("feet");
+    }
+    private void Update()
+    {
+        CheckIsGrounded();
+        Move();
+        ApplyGravity();
+    
+    }
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move);
+    }
+
+    private void CheckIsGrounded()
+    {
+        isGrounded = Physics.CheckSphere(feet.position, groundCheckRadius, groundLayer);
+    }
+
+    private void ApplyGravity()
+    {
+        velocity += Vector3.down * gravity * Time.deltaTime;
+        if (isGrounded)
+            velocity = Vector3.zero;
+
+        controller.Move(velocity);
+    }
+
+}
