219505ad3c7b2cd0ea7992860d63b5266051ad94
Winkhashtag <winkhashtag@gmail.com>
2026-02-05 14:39:24 -0500

    skybox ash

--- Assets/Scripts/playAudio2.cs
+++ Assets/Scripts/playAudio2.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class playAudio2 : MonoBehaviour
 {
-    public float fadeTimeInSeconds;
+    public AudioMixerSnapshot unmutedSnapshot;
+    public AudioMixerSnapshot mutedSnapshot;
+    public float fadeTime;
 
     private AudioSource audio;
+    private AudioMixer mixer;
+    private float[] weights;
+    private AudioMixerSnapshot[] snapshots;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,9 +18,15 @@ public class playAudio2 : MonoBehaviour
     {
 
         audio = GetComponent<AudioSource>();
+        mixer = audio.outputAudioMixerGroup.audioMixer;
 
-       
-
+        snapshots = new AudioMixerSnapshot[]
+        {
+            unmutedSnapshot, //index 0
+            mutedSnapshot // index 1
+        };
+        //make an array of size 2
+        weights = new float[2];
     }
 
     // Update is called once per frame
@@ -27,9 +39,10 @@ public class playAudio2 : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            StopAllCoroutines();
+            weights[0] = 1;  //turn muted snapshot ON
+            weights[1] = 0;  //turn muted snapshot OFF
+            mixer.TransitionToSnapshots(snapshots, weights, fadeTime);
 
-          //  StartCoroutine(FadeAudio(true));
         }
     }
 
@@ -37,9 +50,10 @@ public class playAudio2 : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            StopAllCoroutines();
-            audio.Stop();
-         //   StartCoroutine(FadeAudio(true));
+            weights[0] = 0;  //turned unmuted OFF
+            weights[1] = 1;  // turn unmuted ON
+            mixer.TransitionToSnapshots(snapshots, weights, fadeTime);
+
         }
     }
 }
